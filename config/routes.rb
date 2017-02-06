59f45f4cd69408ffc133458d7513e22be5f02af1
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-06 18:54:33 -0500

    Writing the review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'users/new'
-
-  get 'users/create'
+  # get 'sessions/new'
+  #
+  # get 'sessions/create'
+  #
+  # get 'sessions/destroy'
+  #
+  # get 'users/new'
+  #
+  # get 'users/create'
 
 # get 'products/index'
 # get 'products/show'
 # get 'products/new'
 # get 'products/edit'
+resources :products do
+  resources :reviews, only: [:show, :create, :destroy]
+end
+  resources :users, only: [:new, :create]
 
-resources :products
-
-resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
 
 end
