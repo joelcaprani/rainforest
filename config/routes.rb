d09430e43d0ac1a973b07e3e10d07d6d7d90e5b5
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-06 17:05:00 -0500

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
 # get 'products/index'
 # get 'products/show'
 # get 'products/new'
 # get 'products/edit'
 
 resources :products
+
+resources :users, only: [:new, :create]
+
 
 end
